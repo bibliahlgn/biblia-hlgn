c6f621dbe7e656af64851ba961f9a24aa6130e74
bibliahlgn <bibliahlgn+gh@hotmail.com>
2024-07-26 19:10:58 +0800

    Booklist

--- src/components/bookList.tsx
+++ src/components/bookList.tsx
@@ -1,3 +1,39 @@
+import { useState } from "react";
+
 export function BookList() {
-  return "HI";
+  const booknames = {
+    old: ["Genesis50", "Exodus40"],
+    new: ["Mateo28", "Marcos16", "Lucas24"],
+  };
+
+  const [testament, setTestament] = useState("old");
+
+  const bookName = (i: string) => i.replace(/\d+$/, "");
+
+  return (
+    <>
+      <div>
+        <button onClick={() => setTestament("old")}>Old</button>
+        <button onClick={() => setTestament("new")}>New</button>
+      </div>
+      <ul
+        data-isnew={testament === "new" ? "true" : "false"}
+        className="data-isnew:hidden"
+      >
+        {booknames.old.map((i) => (
+          <li key={i.toLowerCase()} className="cursor-pointer">
+            {bookName(i)}
+          </li>
+        ))}
+      </ul>
+      <ul
+        data-isnew={testament === "new" ? "true" : "false"}
+        className="data-isnew:block hidden"
+      >
+        {booknames.new.map((i) => (
+          <li key={i.toLowerCase()}>{bookName(i)}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
