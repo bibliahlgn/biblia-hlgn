5944e7fb4ddd0b24a20bb2d44dec45bec1eae663
bibliahlgn <bibliahlgn+gh@hotmail.com>
2024-07-27 15:33:56 +0800

    Modified the testament toggle button
    Used only one button for new and old, instead of two separate buttons
    
    Made it like a switch button

--- src/components/bookList.tsx
+++ src/components/bookList.tsx
@@ -1,8 +1,6 @@
 import books from "../data/booklist.json";
 import { useEffect, useState } from "react";
 
-type testamentTYPE = "old" | "new";
-
 export default function BookList({
   chapterCount,
   setChapterCount,
@@ -10,16 +8,21 @@ export default function BookList({
   chapterCount: number;
   setChapterCount: React.Dispatch<React.SetStateAction<number>>;
 }) {
-  const [testament, setTestament] = useState<testamentTYPE>(() => {
+  const [isNewTestament, setNewTestament] = useState<boolean>(() => {
     const selectedTestament = sessionStorage.getItem("OpenTESTAMENT");
-    return selectedTestament === "old" || selectedTestament === "new"
-      ? selectedTestament
-      : "old";
+    return selectedTestament ? JSON.parse(selectedTestament) : false;
   });
 
+  const toggleTestament = () => {
+    setNewTestament((prevState) => !prevState);
+    if (chapterCount !== 0) {
+      setChapterCount(0);
+    }
+  };
+
   useEffect(() => {
-    sessionStorage.setItem("OpenTESTAMENT", testament);
-  }, [testament]);
+    sessionStorage.setItem("OpenTESTAMENT", JSON.stringify(isNewTestament));
+  }, [isNewTestament]);
 
   const [openBook, setOpenBook] = useState("");
 
@@ -38,31 +41,40 @@ export default function BookList({
   return (
     <>
       <div>
-        <button onClick={() => setTestament("old")}>Old</button>
-        <button onClick={() => setTestament("new")}>New</button>
-      </div>
-      <ul
-        data-isnew={testament === "new" ? "true" : "false"}
-        className="data-isnew:hidden"
-      >
-        {books.old.map((i) => (
-          <li
-            key={i.toLowerCase()}
-            onClick={() => toggleChapters(i)}
-            className="cursor-pointer"
+        <button onClick={() => toggleTestament()}>
+          <span
+            data-isnew={isNewTestament ? "true" : "false"}
+            className="bg-slate-400 data-isnew:bg-transparent"
           >
-            {bookName(i)}
-          </li>
-        ))}
-      </ul>
-      <ul
-        data-isnew={testament === "new" ? "true" : "false"}
-        className="hidden data-isnew:block"
-      >
-        {books.new.map((i) => (
-          <li key={i.toLowerCase()}>{bookName(i)}</li>
-        ))}
-      </ul>
+            Old
+          </span>
+          <span
+            data-isnew={isNewTestament ? "true" : "false"}
+            className="bg-transparent data-isnew:bg-slate-400"
+          >
+            New
+          </span>
+        </button>
+      </div>
+      {!isNewTestament ? (
+        <ul>
+          {books.old.map((i) => (
+            <li
+              key={i.toLowerCase()}
+              onClick={() => toggleChapters(i)}
+              className="cursor-pointer"
+            >
+              {bookName(i)}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <ul>
+          {books.new.map((i) => (
+            <li key={i.toLowerCase()}>{bookName(i)}</li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
