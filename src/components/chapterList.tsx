a93e624ab1ee4f9bed743c3b7ea2cf3dce150546
bibliahlgn <bibliahlgn+gh@hotmail.com>
2024-07-28 11:57:14 +0800

    - Added the content page  -- Fully working

--- src/components/chapterList.tsx
+++ src/components/chapterList.tsx
@@ -1,14 +1,24 @@
 export default function ChapterList({
   chapterCount,
+  openBook,
+  setBookToOpen,
 }: {
   chapterCount: number;
+  openBook: string;
+  setBookToOpen: React.Dispatch<React.SetStateAction<string>>;
 }) {
+  const setWhatToOpen = (e: React.MouseEvent<HTMLLIElement>) => {
+    setBookToOpen(
+      `${openBook.toLowerCase()}${(e.target as HTMLLIElement).textContent}`,
+    );
+  };
+
   const renderChapterButtons = () => {
     let items = [];
 
     for (let i = 1; i <= chapterCount; i++) {
       items.push(
-        <li key={i} className="cursor-pointer">
+        <li key={i} className="cursor-pointer" onClick={setWhatToOpen}>
           {i}
         </li>,
       );
