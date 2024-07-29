71682b623dc247acc6c4443aa03dc0a970c50377
bibliahlgn <bibliahlgn+gh@hotmail.com>
2024-07-29 16:31:25 +0800

    Separate file for list toggle buttons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import { activeListTYPES } from "../types";
+import { toggleBookList, toggleChapterList } from "../utils/toggleList";
 
 function Header({
   bookListText,
@@ -9,25 +10,16 @@ function Header({
   selectedChapter: string;
   setActiveList: React.Dispatch<React.SetStateAction<activeListTYPES>>;
 }) {
-  const openChapterList = () => {
-    setActiveList((prevState) => ({
-      bookList: prevState.bookList ? !prevState.bookList : prevState.bookList,
-      chapterList: !prevState.chapterList,
-    }));
-  };
-
   return (
     <header className="flex justify-between px-4 py-3">
       <div className="flex gap-2">
-        <button
-          onClick={() =>
-            setActiveList((prevState) => ({ bookList: !prevState.bookList }))
-          }
-        >
+        <button onClick={() => toggleBookList({ setActiveList })}>
           {bookListText}
         </button>
 
-        <button onClick={() => openChapterList()}>{selectedChapter}</button>
+        <button onClick={() => toggleChapterList({ setActiveList })}>
+          {selectedChapter}
+        </button>
       </div>
       <div className="flex gap-x-2">
         <button>Search</button>
