f2c59980c2f8ee83a8981577352beabdbc9caedf
bibliahlgn <bibliahlgn+gh@hotmail.com>
2024-08-05 10:55:10 +0800

    Responsiveness to large screen size
    and other changes

--- src/components/contents.tsx
+++ src/components/contents.tsx
@@ -1,57 +1,15 @@
-import React, { useEffect } from "react";
-import DOMPurify from "dompurify";
-import { bookToOpenTYPES } from "../types";
 import About from "./about";
 
 export default function Contents({
   rawContent,
-  setRawContent,
-  pathFragments,
   activeAbout,
 }: {
   rawContent: string;
-  setRawContent: React.Dispatch<React.SetStateAction<string>>;
-  pathFragments: bookToOpenTYPES;
   activeAbout: boolean;
 }) {
-  useEffect(() => {
-    const fetchBook = async () => {
-      if (
-        pathFragments.bookName != "" &&
-        pathFragments.chapter != "" &&
-        pathFragments.testament != null
-      ) {
-        const pathToBook = `books/${pathFragments.testament.toLowerCase()}/${pathFragments.bookName?.toLowerCase()}/${pathFragments.bookName?.toLowerCase()}${pathFragments.chapter}.htm`;
-
-        const resp = await fetch(pathToBook);
-        const raw = await resp.text();
-
-        const purifiedHTML = DOMPurify.sanitize(raw, {
-          ALLOWED_TAGS: [
-            "h1",
-            "h2",
-            "h3",
-            "h4",
-            "h5",
-            "h6",
-            "p",
-            "strong",
-            "sup",
-          ],
-          USE_PROFILES: {
-            html: true,
-          },
-        });
-
-        setRawContent(purifiedHTML);
-      }
-    };
-    fetchBook();
-  }, [pathFragments.chapter]);
-
   return activeAbout ? (
     <About></About>
   ) : (
-    <article dangerouslySetInnerHTML={{ __html: rawContent }}></article>
+    <div dangerouslySetInnerHTML={{ __html: rawContent }}></div>
   );
 }
